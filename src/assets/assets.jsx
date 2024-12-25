f0c46ee537949082d3764cd09e7d7765c5512a4b
Bappa Barik <61709328+bappabarik@users.noreply.github.com>
2024-12-25 16:24:28 +0530

    all logos and icons imported in assests and exported the assets aboject

---
+++ src/assets/assets.jsx
@@ -0,0 +1,58 @@
+//import all logos
+import AntLogo from "./logos/AntLogo.svg";
+import AntLogoWatermark from "./logos/AntLogoWatermark.svg";
+import GitLabLogo from "./logos/GitLabLogo.svg";
+import { GrGithub as GitHubLogo } from "react-icons/gr";
+import { FaBitbucket as BitbucketLogo } from "react-icons/fa6";
+import { VscAzureDevops as AzureDevopsLogo } from "react-icons/vsc";
+import { LuKeyRound as SSOLogo } from "react-icons/lu";
+import CodeAntAILogo from "./logos/CodeAntAILogo.svg";
+
+//import all icons
+import PieChartIcon from "./icons/PieChartIcon.svg";
+import { AiOutlineHome as homeIcon } from "react-icons/ai";
+import { IoCodeSlashOutline as codeIcon } from "react-icons/io5";
+import { HiOutlineCloud as cloudIcon } from "react-icons/hi2";
+import { LuBookText as bookIcon } from "react-icons/lu";
+import { IoSettingsOutline as settingsIcon } from "react-icons/io5";
+import { TbDatabase as dbIcon } from "react-icons/tb";
+import { IoCallOutline as callIcon } from "react-icons/io5";
+import { LuLogOut as logoutIcon } from "react-icons/lu";
+import { FiRefreshCw as refreshIcon } from "react-icons/fi";
+import { LuPlus as plusIcon } from "react-icons/lu";
+import { GoSearch as searchIcon } from "react-icons/go";
+import { FaChevronDown as angleDownIcon } from "react-icons/fa6";
+import { RxHamburgerMenu as menuIcon } from "react-icons/rx";
+import { RxCross2 as crossIcon } from "react-icons/rx";
+
+const assets = {
+  logos: {
+    AntLogo,
+    AntLogoWatermark,
+    GitLabLogo,
+    GitHubLogo,
+    BitbucketLogo,
+    AzureDevopsLogo,
+    SSOLogo,
+    CodeAntAILogo,
+  },
+  icons: {
+    homeIcon,
+    codeIcon,
+    cloudIcon,
+    bookIcon,
+    settingsIcon,
+    dbIcon,
+    callIcon,
+    logoutIcon,
+    refreshIcon,
+    plusIcon,
+    searchIcon,
+    angleDownIcon,
+    menuIcon,
+    crossIcon,
+    PieChartIcon,
+  },
+};
+
+export default assets;
